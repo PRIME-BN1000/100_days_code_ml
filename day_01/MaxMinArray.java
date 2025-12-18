dc619d5e596bc3d7733defb37c92db426609a4c0
Prinjal Mistry <prinjal06@gmail.com>
2025-12-18 12:54:18 +0530

    Add newline after printing array elements in MaxMinArray

--- day_01/MaxMinArray.java
+++ day_01/MaxMinArray.java
@@ -22,6 +22,7 @@ public class MaxMinArray {
         }
          for(i=0;i<n;i++)
             System.out.print(arr[i]+" ");
+        System.out.println();
         System.out.println("Maximum element is : "+max);
         System.out.println("Minimum element is : "+min);
     }
