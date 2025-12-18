05c1c47b3c4c31690050eb60467adbe6ca4402cd
Prinjal Mistry <prinjal06@gmail.com>
2025-12-18 13:40:06 +0530

    Add SecondLargest class to find the second largest element in an array

---
+++ day_01/SecondLargest.java
@@ -0,0 +1,30 @@
+package day_01;
+
+import java.util.*;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[], n, max, max2;
+        System.out.print("Enter the size of array: ");
+        n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element : ");
+            arr[i] = sc.nextInt();
+        }
+        max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        max2 = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max2 && arr[i] < max) {
+                max2 = arr[i];
+            }
+        }
+        System.out.println("Second largest element is: " + max2);
+    }
+}
