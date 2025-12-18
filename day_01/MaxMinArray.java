2c76dfad63f3a918b09e6b90f9390118a17cf44f
Prinjal Mistry <prinjal06@gmail.com>
2025-12-18 12:24:57 +0530

    Fiding Maximum and Minimum in an Array

---
+++ day_01/MaxMinArray.java
@@ -0,0 +1,26 @@
+package day_01;
+
+import java.util.Scanner;
+
+public class MaxMinArray {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        int arr[], n, max, min, i;
+        System.out.println("Enter the size of the array : ");
+        n=sc.nextInt();
+        arr=new int[n];
+        for(i=0;i<n;i++){
+            System.out.println("Enter an element : ");
+            arr[i]=sc.nextInt();
+        }
+        max=min=arr[0];
+        for(i=1;i<n;i++){
+            if(max<arr[i])
+                max=arr[i];
+            if(min>arr[i])
+                min=arr[i];
+        }
+        System.out.println("Maximum element is : "+max);
+        System.out.println("Minimum element is : "+min);
+    }
+}
