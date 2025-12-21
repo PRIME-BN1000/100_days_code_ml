aba9af0359d8715e01732566b8521d0a418406ab
Prinjal Mistry <prinjal06@gmail.com>
2025-12-21 19:44:32 +0530

    max_element  in a 2D array

---
+++ day_03/max_element.java
@@ -0,0 +1,30 @@
+package day_03;
+
+import java.util.*;
+
+public class max_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[][], r, c;
+        System.out.print("Enter the number of rowws: ");
+        r = sc.nextInt();
+        System.out.print("Enter the number of columns: ");  
+        c = sc.nextInt();
+        a = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print("Enter the element : ");
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int max = a[0][0];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (a[i][j] > max) {
+                    max = a[i][j];
+                }
+            }
+        }
+        System.out.println("The maximum element in the array is: " + max);
+    }
+}
