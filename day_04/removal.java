e9df916b62cc7ae4dc69c4737713c33d8c907752
Prinjal Mistry <prinjal06@gmail.com>
2025-12-22 21:21:04 +0530

    Add removal program to filter out specified letters from a sentence

---
+++ day_04/removal.java
@@ -0,0 +1,22 @@
+package day_04;
+
+import java.util.*;
+
+public class removal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.err.println("Enter a sentence:");
+        String sentence = scanner.nextLine();
+        System.err.println("Enter a letter to remove:");
+        char letterToRemove = scanner.nextLine().charAt(0);
+        int l=sentence.length();
+        for(int i=0;i<l;i++){
+            if(sentence.charAt(i)==letterToRemove){
+                sentence=sentence.substring(0,i)+sentence.substring(i+1);
+                i--;
+                l--;
+            }
+        }
+        System.out.println("Filtered sentence: " + sentence);
+    }
+}
