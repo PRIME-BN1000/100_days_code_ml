aef2e8375d9f6306a1297bef21f14ba2fb708771
Prinjal Mistry <prinjal06@gmail.com>
2025-12-22 22:14:58 +0530

    Add non_repeating_string program to identify non-repeating characters in a string

---
+++ day_04/non_repeating_string.java
@@ -0,0 +1,20 @@
+package day_04;
+
+import java.util.*;
+
+public class non_repeating_string {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        String result = "";
+        int len = input.length();
+        for (int i = 0; i < len; i++) {
+            char ch = input.charAt(i);
+            if (input.indexOf(ch) == input.lastIndexOf(ch)) {
+                result = result + ch;
+            }
+        }
+        System.out.println("The non-repeating characters are: " + result);
+    }
+}
