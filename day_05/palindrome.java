e998b387673b3430b95365793721ac61d60e3403
Prinjal Mistry <prinjal06@gmail.com>
2025-12-24 20:29:11 +0530

    Add palindrome program to check if a string is a palindrome usinnnnng recursion

---
+++ day_05/palindrome.java
@@ -0,0 +1,27 @@
+package day_05;
+
+import java.util.Scanner;
+
+public class palindrome {
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        if (isPalindrome(input, 0, input.length() - 1)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        sc.close();
+    }
+
+     public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
+}
