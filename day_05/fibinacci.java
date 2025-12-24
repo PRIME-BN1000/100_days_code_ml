cbe1cb088eb129da44a8bd94ee0c89a90859058d
Prinjal Mistry <prinjal06@gmail.com>
2025-12-24 20:38:14 +0530

    Add Fibonacci program to generate Fibonacci series up to a specified number of terms

---
+++ day_05/fibinacci.java
@@ -0,0 +1,22 @@
+package day_05;
+
+import java.util.Scanner;
+
+public class fibinacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms for Fibonacci series: ");
+        int n = scanner.nextInt();
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        System.out.println();
+        scanner.close();
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
